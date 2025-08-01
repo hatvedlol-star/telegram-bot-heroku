0ac05b6b9836ae92c0250a44f0a9ffa7a53a8fde
hatvedlol-star <hatvedlol@gmail.com>
2025-08-01 10:39:28 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from telegram.ext import Updater, CommandHandler
+import os
 
+TOKEN = os.environ.get("BOT_TOKEN")
+
+def start(update, context):
+    update.message.reply_text("بوت حاتم يعمل على Heroku! 🚀")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+dp.add_handler(CommandHandler("start", start))
+
+updater.start_polling()
+updater.idle()
